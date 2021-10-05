3e0c4357e2aeddc691acb2396743c009f1f44c22
Lenny <mac@Macs-MacBook-Air.local>
2021-10-05 11:57:00 +0300

    Survery Page

--- js/index.js
+++ js/index.js
@@ -26,3 +26,13 @@ INSTRUCTIONS:
 
 // Once you have read the above messages, you can delete all comments. 
 "use strict";
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
